c857f04ea73e0de656b84f6ef73a6d46778ea80b
iDhruv11 <drvsrm11@gmail.com>
2026-02-04 10:54:45 +0530

    add ui

--- src/main/java/portshare/App.java
+++ src/main/java/portshare/App.java
@@ -1,7 +1,28 @@
 package portshare;
 
+import portshare.controller.FileController;
+import java.io.IOException;
+
 public class App {
   public static void main(String[] args) {
-    System.out.println("Hello World!");
+    try {
+      FileController fileController = new FileController(8080);
+      fileController.start();
+
+      System.out.println("Portshare server started on port 8080");
+      System.out.println("UI available at http://localhost:3000");
+
+      Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+        System.out.println("Shutting down server...");
+        fileController.stop();
+      }));
+
+      System.out.println("Press Enter to stop the server");
+      System.in.read();
+
+    } catch (IOException e) {
+      System.err.println("Error starting server: " + e.getMessage());
+      e.printStackTrace();
+    }
   }
 }
